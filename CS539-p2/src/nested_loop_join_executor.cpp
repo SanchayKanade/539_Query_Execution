068bd2914fb44c70b587e4a92c37b00a91cd3117
SanchayKanade <sanchay2598@gmail.com>
2022-11-27 16:45:25 -0500

    Nested loop join executor implemented

--- CS539-p2/src/nested_loop_join_executor.cpp
+++ CS539-p2/src/nested_loop_join_executor.cpp
@@ -1,4 +1,8 @@
 #include "../include/nested_loop_join_executor.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 NestedLoopJoinExecutor::NestedLoopJoinExecutor(
     AbstractExecutor *left_child_executor,
@@ -7,6 +11,66 @@ NestedLoopJoinExecutor::NestedLoopJoinExecutor(
       right_(right_child_executor),
       join_key_(join_key){};
 
-void NestedLoopJoinExecutor::Init() {}
+void NestedLoopJoinExecutor::Init() {
+  left_->Init();
+  right_->Init();
 
-bool NestedLoopJoinExecutor::Next(Tuple *tuple) { return false; }
+}
+
+bool NestedLoopJoinExecutor::Next(Tuple *tuple) { 
+  //Object of class tuple
+  Tuple tuple_left;
+  Tuple tuple_right;
+
+  //Case when join_key_ is id
+  if(join_key_ == "id"){
+    while(left_->Next(&tuple_left)){
+      while(right_->Next(&tuple_right)){
+        if(tuple_left.id == tuple_right.id){
+          *tuple = tuple_left;
+          return true;
+        }
+      }
+      //Because right executor reaches the bottom of the table then we need to set it again
+      right_->Init();
+    }
+    return false;
+  }
+
+  //Case when join_key_ is val1
+  if(join_key_ == "val1"){
+      while(left_->Next(&tuple_left)){
+        while(right_->Next(&tuple_right)){
+          if(tuple_left.val1 == tuple_right.val1){
+            *tuple = tuple_left;
+            return true;
+          }
+        }
+        //Because right executor reaches the bottom of the table then we need to set it again
+        right_->Init();
+      }
+  return false;
+  }
+
+  //Case when join_key_ is val2
+  else if(join_key_ == "val2"){
+      while(left_->Next(&tuple_left)){
+        while(right_->Next(&tuple_right)){
+          if(tuple_left.val2 == tuple_right.val2){
+            *tuple = tuple_left;
+            return true;
+          }
+        }
+        //Because right executor reaches the bottom of the table then we need to set it again
+        right_->Init();
+      }
+  return false;
+  }
+
+  //Case when join_key_ is not a valid value
+  else{
+    cout<<"Not a valid join_key."<<endl;
+    return false;
+  }
+}
+   
